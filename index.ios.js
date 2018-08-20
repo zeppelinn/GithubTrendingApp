37bf8045442f8a26cd0de82f754e7e929a77c0f7
ljn <467337035@qq.com>
2018-08-20 14:11:41 +0800

    项目底部导航菜单开发

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,71 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
+import TabNavigator from 'react-native-tab-navigator'
+
 export default class GithubTrendingApp extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedTab:"tab_popular"/* 初始化state，默认选中第一个tab */
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_popular'} 
+            selectedTitleStyle={{color:"red"}} /* 标签底部文字的样式 */
+            title="热门"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_popular.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor:"red"}]} source={require('./res/images/ic_popular.png')} />}/* style的属性不仅可以使用一个对象来赋值，还能用一个集合来赋值 */
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'tab_popular' })}>
+            <View style={styles.page}>
+
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab-trending'}
+            selectedTitleStyle={{color:"red"}}
+            title="趋势"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor:"red"}]} source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tab-trending' })}>
+            <View style={styles.page1}>
+
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_favourite'}
+            selectedTitleStyle={{color:"red"}}
+            title="收藏"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor:"red"}]} source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tab_favourite' })}>
+            <View style={styles.page1}>
+
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_my'}
+            selectedTitleStyle={{color:"red"}}
+            title="我的"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor:"red"}]} source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tab_my' })}>
+            <View style={styles.page1}>
+
+            </View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -34,20 +81,20 @@ export default class GithubTrendingApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page: {
+    flex:1,
+    backgroundColor:'blue'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page1: {
+    flex:1,
+    backgroundColor:'green'
   },
+  image: {
+    height:22,
+    width:22
+  }
 });
 
 AppRegistry.registerComponent('GithubTrendingApp', () => GithubTrendingApp);
