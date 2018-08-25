db3d8ad71145feb79cbe83095a03f6a0898eb133
ljn <467337035@qq.com>
2018-08-25 11:41:04 +0800

    实现popular页面的数据填充

---
+++ js/common/RepositoryCell.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    TextInput,
+    ListView
+} from 'react-native';
+
+export default class RepositoryCell extends Component {
+    render() {
+        return (
+            <View style={{margin:10}} >
+                <Text>{this.props.data.full_name}</Text>
+                <Text>{this.props.data.description}</Text>
+                <Text>{this.props.data.owner.avatar_url}</Text>
+                <Text>{this.props.data.stargazers_count}</Text>
+            </View>
+        )
+    }
+}
--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -2,29 +2,62 @@ import React, { Component } from 'react'
 import {
     View,
     Text,
-    TextInput
+    TextInput,
+    ListView
 } from 'react-native';
 
 import NavigationBar from '../common/NavigationBar';
 import HomePage from './HomePage';
 import DataRepository from '../expend/dao/DataRepository'
+import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view';
+import RepositoryCell from '../common/RepositoryCell'
 
 export default class PopularPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+        <View style={styles.container} >
+            <NavigationBar title={'最热'} style={{ backgroundColor:'#6495ed' }} />
+            <ScrollableTabView 
+                renderTabBar={() => <ScrollableTabBar/>}
+            >
+                <PopularTab tabLabel="Java" >Java</PopularTab>
+                <PopularTab tabLabel="Android" >Android</PopularTab>
+                <PopularTab tabLabel="iOS" >iOS</PopularTab>
+                <PopularTab tabLabel="React Native" >React Native</PopularTab>
+            </ScrollableTabView>
+        </View>
+        )
+    }
+}
+
+class PopularTab extends Component {
 
     constructor(props){
         super(props);
         this.dataRepository = new DataRepository() //实例化数据请求对象
+
         this.state={
-            result:""
+            result:"",
+            dataSource:new ListView.DataSource({
+                rowHasChanged:(r1, r2) => r1!==r2
+            })
         }
     }
 
+    componentDidMount = () => {
+        this.getData()
+    }
+
     getData = () => {
-        const url = `https://api.github.com/search/repositories?q=${this.text}&sort=star`
+        const url = `https://api.github.com/search/repositories?q=${this.props.tabLabel}&sort=star`
         this.dataRepository.fetchNetRepository(url)
             .then(result => {
                 this.setState({
-                    result:JSON.stringify(result)
+                    dataSource: this.state.dataSource.cloneWithRows(result.items)
                 })
             })
             .catch(error => {
@@ -34,37 +67,14 @@ export default class PopularPage extends Component {
             })
     }
 
-    render() {
+    render(){
         return (
-        <View style={styles.container} >
-            <NavigationBar
-                title={'最热'}
-                style={{
-                    backgroundColor:'#6495ed'
-                }}
-            >
-            </NavigationBar>
-            <Text style={styles.tips}
-                onPress={() => {
-                    this.getData();
-                }}
-            >
-                获取数据
-            </Text>
-            <TextInput
-                style={{
-                    height:20,
-                    borderWidth:1
-                }}
-                onChangeText={(text) => this.text = text}
-            >
-            </TextInput>
-            <Text style={{
-                height:500
-            }} >
-                {this.state.result}
-            </Text>
-        </View>
+            <View>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(data) => <RepositoryCell data={data} />}
+                />
+            </View>
         )
     }
 }
