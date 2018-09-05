35fe8e5efa444f19cf9e756af320dfc69fb68f80
ljn <467337035@qq.com>
2018-09-05 15:42:11 +0800

    实现我的标签页面的开发

--- js/pages/util/ViewUtils.js
+++ js/pages/util/ViewUtils.js
@@ -4,6 +4,8 @@ import {
     Text,
     TouchableOpacity,
     Image,
+    TouchableHighlight,
+    StyleSheet
 } from 'react-native';
 
 export default class ViewUtils{
@@ -18,4 +20,50 @@ export default class ViewUtils{
             />
         </TouchableOpacity>
     }
+
+    /**
+     * 获取设置页的Item
+     * @param callBack  单击item的回调
+     * @param icon      左侧图标
+     * @param text      文本
+     * @param tintStyle 图标着色
+     * @param expandableIcon 右侧图标
+     */
+    static getSettingItems(callBack, icon, text, tintStyle, expandableIcon) {
+        return (
+            <TouchableHighlight onPress={callBack} >
+                <View style={styles.setting_item_container} >
+                            <View style={{flexDirection:'row', alignItems:'center'}} >
+                                <Image resizeMode='stretch' style={[{width:16, height:16, marginRight:10}, tintStyle]} source={icon} />
+                                <Text>{text}</Text>
+                            </View>
+                            <Image source={expandableIcon ? expandableIcon : require('../../../res/images/ic_tiaozhuan.png')}
+                                    style={[{
+                                        marginRight:10,
+                                        height:22,
+                                        width:22
+                                    }, {tintColor:'#2196F3'}]}
+                            />
+                        </View>
+            </TouchableHighlight>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    setting_item_container:{
+        backgroundColor:'white',
+        padding:10,
+        height:60,
+        alignItems:'center',
+        justifyContent:'space-between',
+        flexDirection:'row'
+    },
+    item:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:"center",
+        height:60,
+        padding:10
+    }
+})
---
+++ res/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+/**
+ * 全局样式
+ */
+
+ module.exports = {
+     line:{
+         flex:1,
+         height:0.4,
+         opacity:0.5,
+         backgroundColor:'darkgray'
+     },
+     titleContainer:{
+         flex:1,
+         backgroundColor:"#f3f3f4"
+     }
+ }
